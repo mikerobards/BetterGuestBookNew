f82a5decbbd381d729a1c97c4248f951c95398fa
Michael Robards <mikerobards@icloud.com>
2022-03-10 15:48:35 -0500

    create library

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,19 @@
-﻿
+﻿using GuestBookNewLibrary.Models;
 
+// Capture the info about each guest (assumption is at least one guest and unknown maximum)
+// info to capture: First Name, Last Name, Message to host
+// once done, loop through each guest and print their info
+
+
+List<GuestModel> guests = new List<GuestModel>();
+string moreGuestsComing = "";
+
+do
+{
+    GuestModel guest = new GuestModel();
+    Console.Write("What is your first name: ");
+    guest.FirstName = Console.ReadLine();
+
+} while (moreGuestsComing.ToLower() == "yes");
 
 
